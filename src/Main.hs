6145a0de7799d6b53a18a4021903f85c62955518
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-02-14 23:38:49 +0100

    project rename, moving lambda

--- src/Main.hs
+++ src/Main.hs
@@ -1,79 +1,60 @@
 module Main where
 
+import Data.Time.Clock (getCurrentTime, utctDayTime)
 import Terminal
 import Geometry
 import Bitmap
 import Plot
 
--- The picture.
+-- The viewport.
 
-geom :: (Floating u, Ord u, Enum u) => u -> Poly u
-geom r = rotate r (Point 40 40) $ star (Point 40 40) 6 (r*0.5) r
+center :: Point Double
+center = Point 40 40
 
-geom2 :: (Floating u, Ord u, Enum u) => u -> Poly u
-geom2 r = rotate r (Point 40 40) $ star (Point 40 40) 6 (r*2)   r
+screen :: Rect Integer
+screen = Rect (Point 1 1) (Point 80 40)
 
-geom3 :: (Floating u, Ord u, Enum u) => u -> Poly u
-geom3 r = rotate r (Point 40 40) $ star (Point 40 40) 6  r      r
-
-myBmp :: Double -> Bitmap Double Pixel
-myBmp r =
-    drawPoly (rotate r (Point 40 40) lambda) (Pixel 'x' blue)
-  $ empty
-
-lambda = Poly [
-    Point 25 22
-  , Point 24 22
-  , Point 25 15
-  , Point 27 13
-  , Point 33 13
-  , Point 36 16
-  , Point 37 18
-  , Point 49 53
-  , Point 52 57
-  , Point 56 57
-  , Point 58 55
-  , Point 58 53
-  , Point 60 53
-  , Point 60 58
-  , Point 59 61
-  , Point 57 63
-  , Point 55 64
-  , Point 52 64
-  , Point 50 63
-  , Point 48 60
-  , Point 41 41
-  , Point 30 63
-  , Point 22 63
-  , Point 37 29
-  , Point 36 25
-  , Point 35 22
-  , Point 34 20
-  , Point 31 18
-  , Point 28 19
-  , Point 25 21
-  , Point 25 22
-  ]
-
-
-
-
+view :: Rect Double
+view = Rect (Point 1 1) (Point 80 80)
 
 -- The renderer.
 
 main :: IO ()
-main = loop 0.0
-
-loop :: Double -> IO ()
-loop r = do
+main = do
+  tick <- getCurrentTime >>= return . fromRational . toRational . utctDayTime
   putStr $ move 1 (1::Int)
-  putStrLn $ plot (myBmp r)
-  loop (r + 0.1)
+  putStrLn $ plot (myBmp tick)
+  main
 
 plot :: Bitmap Double Pixel -> String
 plot = string False screen (Point 1 1) " " "" . list 0.5 view
-  where
-    screen = Rect (Point 1 1) (Point 80 40)
-    view   = Rect (Point 1 1) (Point 80 80)
 
+-- The picture.
+
+myBmp :: Double -> Bitmap Double Pixel
+myBmp t =
+    drawPoly (lambda   t) (Pixel 'x' yellow)
+  $ drawPoly (darkstar t) (Pixel '@' black)
+  $ empty
+
+darkstar :: Double -> Poly Double
+darkstar t = 
+    scale (1.2 + 0.6 * sin (t/2)) center
+  $ rotate (-t*2) center
+  $ star center 5 40 18
+
+lambda :: Double -> Poly Double
+lambda t =
+    scale (0.7 + 0.6 * sin (t/4)) center
+  $ rotate t center
+  $ Poly [
+    Point 25 22 , Point 24 22 , Point 25 15 , Point 27 13
+  , Point 33 13 , Point 36 16 , Point 37 18 , Point 49 53
+  , Point 52 57 , Point 56 57 , Point 58 55 , Point 58 53
+  , Point 60 53 , Point 60 58 , Point 59 61 , Point 57 63
+  , Point 55 64 , Point 52 64 , Point 50 63 , Point 48 60
+  , Point 41 41 , Point 30 63 , Point 22 63 , Point 37 29
+  , Point 36 25 , Point 35 22 , Point 34 20 , Point 31 18
+  , Point 28 19 , Point 25 21 , Point 25 22
+  ]
 
